4767b658c600f84d70d2648ba2b4130524123d5f
fpeng-02 <fpeng2002@gmail.com>
2021-04-10 16:05:08 -0700

    Update discordbot.js

--- Main/discordbot.js
+++ Main/discordbot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+
 client.on("ready", () => {
   console.log('Logged in as ${client.user.tag}!')
 })
@@ -16,17 +17,48 @@ client.on("message", msg => {
     msg.reply(msg.author.presence.activities);
   }
 })
-
+/*
 client.on("presenceUpdate", (oldPres, newPres) => {
   oldPres.user.createDM().then( dmCh => {
     dmCh.send("Presence Changed, " + oldPres.activities[0].name + 
-      ", "+ oldPres.activities[0].details + " to " + 
+      ", "+ oldPres.activities[0].type + " to " + 
       newPres.activities[0].name + 
-      ", "+ newPres.activities[0].details);
-    
+      ", "+ newPres.activities[0].type);
   }).catch(error => {
     console.log(error + ", Error in creating DM");
   });
 })
+*/
+
+client.on("presenceUpdate", (oldPres, newPres) => {
+  isLobby = false;
+  //assign the changing 
+  oldActivity = null;
+  newActivity = null;
+  for (i = 0; i < newPres.activities.length; i++){
+    if (newPres.activities[i].type == "PLAYING"){
+      oldActivity = oldPres.activities[i];
+      newActivity = newPres.activities[i];
+      break;
+    }
+  }
+  if (newActivity == null){
+    return;
+  }
+  console.log(newActivity.state);
+
+  if (newActivity.state == null){
+    printStretch(newPres.user);
+  }
+
+})
+
+function printStretch(userDM){
+  userDM.createDM().then( dmCh => {
+    dmCh.send("Time to Stretch");
+  }).catch(error => {
+    console.log(error + ", Error in creating DM");
+  });
+}
 
 client.login('ODMwMzEwMzA5NzY3Njc1OTc0.YHE0vA.3UaDnhS1L72oiJZjyQsvrkC6Dzg');
