9f970f31d75bd612d897a6a30c1776df508eb601
fpeng-02 <fpeng2002@gmail.com>
2021-04-10 13:09:38 -0700

    Added testing for checking and displaying status

--- Main/discordbot.js
+++ Main/discordbot.js
@@ -11,4 +11,22 @@ client.on("message", msg => {
   }
 })
 
+client.on("message", msg => {
+  if (msg.content === "act") {
+    msg.reply(msg.author.presence.activities);
+  }
+})
+
+client.on("presenceUpdate", (oldPres, newPres) => {
+  oldPres.user.createDM().then( dmCh => {
+    dmCh.send("Presence Changed, " + oldPres.activities[0].name + 
+      ", "+ oldPres.activities[0].details + " to " + 
+      newPres.activities[0].name + 
+      ", "+ newPres.activities[0].details);
+    
+  }).catch(error => {
+    console.log(error + ", Error in creating DM");
+  });
+})
+
 client.login('ODMwMzEwMzA5NzY3Njc1OTc0.YHE0vA.3UaDnhS1L72oiJZjyQsvrkC6Dzg');
