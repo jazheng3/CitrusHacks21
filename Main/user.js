46832c54caa65252563071bd2998fb5bbdff0339
Alvin Zhang <alvin.zhang295@gmail.com>
2021-04-10 15:50:34 -0500

    Update user.js

---
+++ Main/user.js
@@ -0,0 +1,13 @@
+class User {
+    constructor(discordusername, discordID, timeActivityStart, timeActivityEnd, numStretches, numWaterBreaks) {
+        this.discordusername = discordusername;
+        this.discordID = discordID;
+        this.timeActivityStart = timeActivityStart; 
+        this.timeActivityEnd = timeActivityEnd;
+        this.numStretches = numStretches; 
+        this.numWaterBreaks = numWaterBreaks;
+    }
+
+    
+}
+
